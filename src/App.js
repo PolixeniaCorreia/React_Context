4fb4f4f9b99b4344f20a20888c47769b8232db79
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-14 16:11:19 -0300

    Exibindo no Console

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [todos, setTodos] = useState([
-        { id: 1, title: 'Ir ao supermercado', done: false}
-
+        { id: 1, title: 'Ir ao supermercado', done: false },
+        { id: 2, title: 'Ir para Academia', done: false },
+        { id: 3, title: 'Passear com o cachorro', done: false },
     ]);
-    return(
+    const [todo,setTodo] = useState();
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+    }
+    
+    const handleInputChange = e => {
+        console.log(e.target.value);
+    }
+    
+
+
+    return (
         <div>
             {
-                todos.map(i =>(
-                    <div key= {i.id}>{i.title}</div>
+                todos.map(i => (
+                    <div key={i.id}>{i.title}</div>
                 ))
             }
+            <br />
+            <hr />
+            <br />
+            <form>
+                <input type="text" name="todo"
+                 id="title" placeholder="Nova tarefa.."
+                 onChange={handleInputChange} />
+
+                <button>Adicionar</button>
+            </form>
+
         </div>
     );
 }
- 
+
 export default App;
