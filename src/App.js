5195d22ae693d9bf7b2ea2acf1bc342c4e78941d
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-15 10:39:49 -0300

    Usando Context

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import TodoProvider from './contexts/TodoContext';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo'; 
 
 const App = () => {
-    const [todos, setTodos] = useState([
-        { id: 1, title: 'Ir ao supermercado', done: false },
-        { id: 2, title: 'Ir para Academia', done: false },
-        { id: 3, title: 'Passear com o cachorro', done: false },
-    ]);
-    const [todo,setTodo] = useState();
-
-    const handleFormSubmit = e => {
-        e.preventDefault();
-        setTodos([
-            ...todos,
-            todo,
-        ])
-    }
-    
-    const handleInputChange = e => {
-        setTodo({
-            ...todo,
-            id: todos.length + 1,
-            title: e.target.value,
-            done: false
-        })
-    }
-    
-
 
     return (
-        <div>
-            {
-                todos.map(i => (
-                    <div key={i.id}>{i.title}</div>
-                ))
-            }
-            <br />
-            <hr />
-            <br />
-            <form onSubmit={handleFormSubmit}>
-                <input type="text" name="todo"
-                 id="title" placeholder="Nova tarefa.."
-                 onChange={handleInputChange} />
-
-                <button>Adicionar</button>
-            </form>
-
-        </div>
+        <TodoProvider>
+            <TodoList></TodoList>
+            <br/>
+            <hr/>
+            <br/>
+            <AddTodo></AddTodo>
+        </TodoProvider>
     );
 }
 
